87459c0f9a6866ef0cd33118462e2c2934aac819
Cerek Hillen <crockeo@gmail.com>
2015-10-06 14:41:17 -0400

    Starting to implement models.

--- src/res/model.h
+++ src/res/model.h
@@ -1,42 +1,39 @@
-#include "model.h"
+#ifndef _LENTIL_RESO_MODEL_H_
+#define _LENTIL_RESO_MODEL_H_
 
 //////////////
 // Includes //
-#include <stdlib.h>
+#include "../errors.h"
 
 //////////
 // Code //
 
+// Information to represent a model resource.
+typedef struct {
+
+} Lentil_Reso_Model;
+
 // Constructing a new Lentil_Reso_Model on the heap.
-Lentil_Reso_Model* createModel() {
-    return NULL;
-}
+Lentil_Reso_Model* createModel();
 
 // Freeing up all of the memory from a given model.
-void Lentil_Reso_destroyModel(Lentil_Reso_Model* model) {
-}
+void Lentil_Reso_destroyModel(Lentil_Reso_Model*);
 
 // Loading a .obj file into a Lentil_Reso_Model.
-void Lentil_Reso_loadObj(const char* path, Lentil_Reso_Model* model, Lentil_Core_Error* pErr) {
-
-}
+void Lentil_Reso_loadObj(const char*, Lentil_Reso_Model*, Lentil_Core_Error*);
 
 // Saving a Lentil_Reso_Model to a given location on disk in the format of a
 // .obj.
-void Lentil_Reso_saveObj(const char* path, Lentil_Reso_Model* model, Lentil_Core_Error* pErr) {
-
-}
+void Lentil_Reso_saveObj(const char*, Lentil_Reso_Model*, Lentil_Core_Error*);
 
 // Loading a custom .lent file into a Lentil_Reso_Model. A .lent file is a
 // custom file format to store 3D models in a smaller, more efficient form than
 // a generic .obj file.
-void Lentil_Reso_loadLent(const char* path, Lentil_Reso_Model* model, Lentil_Core_Error* pErr) {
-
-}
+void Lentil_Reso_loadLent(const char*, Lentil_Reso_Model*, Lentil_Core_Error*);
 
 // Saving a Lentil_Reso_Model to a given location on disk in the format of a
 // .lent. Refer to the documentation for Lentil_Reso_loadLent(...) for what a
 // .lent file is.
-void Lentil_Reso_saveLent(const char* path, Lentil_Reso_Model* model, Lentil_Core_Error* pErr) {
+void Lentil_Reso_saveLent(const char*, Lentil_Reso_Model*, Lentil_Core_Error*);
 
-}
+#endif
