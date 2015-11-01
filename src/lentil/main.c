07a20e70606e2d98d452bb4155fd09f14378434a
Cerek Hillen <crockeo@gmail.com>
2015-11-01 11:21:58 -0500

    Let the user choose what model to view at run time.

--- src/lentil/main.c
+++ src/lentil/main.c
@@ -4,6 +4,7 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 
+#include "flags.h"
 #include "core.h"
 #include "rend.h"
 #include "reso.h"
@@ -65,22 +66,22 @@ GLFWwindow* initialize() {
 }
 
 // Running the game with the created GLFWwindow.
-void run(GLFWwindow* window, const char* modelLoc) {
+void run(GLFWwindow* window, Lentil_ResConfig cfg) {
     // Choosing an appropriate debug level.
     Lentil_Core_debugLevel(3);
 
     // Testing a shader load.
     Lentil_Core_Error shaderErr = Lentil_Core_defaultError();
-    GLuint program = Lentil_Reso_loadShaderProgram("res/shaders/test", &shaderErr);
+    GLuint program = Lentil_Reso_loadShaderProgram(cfg.shaderPath, &shaderErr);
 
     // Testing a texture load.
     Lentil_Core_Error textureErr = Lentil_Core_defaultError();
-    GLuint texture = Lentil_Reso_loadTexture("res/imgs/gradient.png", &textureErr);
+    GLuint texture = Lentil_Reso_loadTexture(cfg.texturePath, &textureErr);
 
     // Testing a model load.
     Lentil_Core_Error modelErr = Lentil_Core_defaultError();
     Lentil_Reso_Model* model = Lentil_Reso_Model_new();
-    Lentil_Reso_loadObjModelStr(modelLoc, model, &modelErr);
+    Lentil_Reso_loadObjModelStr(cfg.modelPath, model, &modelErr);
 
     // Testing a model render.
     Lentil_Core_Error mrErr = Lentil_Core_defaultError();
@@ -196,15 +197,13 @@ void destroy(GLFWwindow* window) {
 }
 
 // The entry point to the application.
-int main(int argc, char** argv) {
+int main(int argc, const char** argv) {
+    Lentil_ResConfig cfg = Lentil_parseFlags(argc, argv);
+
     GLFWwindow* window = initialize();
     if (window == NULL)
         return 1;
-
-    if (argc > 1)
-        run(window, argv[1]);
-    else
-        run(window, "res/models/test.obj");
+    run(window, cfg);
     destroy(window);
 
     return 0;
