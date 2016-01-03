9bfc3dfc0495571ed519e45e36ebc8c627f32daf
Cerek Hillen <crockeo@gmail.com>
2016-01-02 19:07:22 -0500

    Added the ability to remove items from arrays (untested).

--- src/lentil/core/array.h
+++ src/lentil/core/array.h
@@ -40,4 +40,24 @@
         array = narr; } \
     array[array ## Length++] = value;
 
+// WARNING: This is probably really bad practice. That being said, it's really
+// useful.
+//
+// - array : needs to be the name of some value that is a pointer, and has the
+//   definitions of Length and Size as ints under the name of those strings
+//   appended to the variable defined by array.
+//
+// - type : needs to be name that represents the type of the value you're trying
+//   to add. For instance if you're trying to add a Lentil_Reso_Model_Face, then
+//   type needs to be Lentil_Reso_Model_Face.
+#define Lentil_Core_removeElement(array, type) \
+    if (array ## Length <= array ## Size / 4) \
+        type* narr = malloc(array ## Size / 2 * sizeof(type)); \
+        for (int i = 0; i < array ## Length; i++) \
+            narr[i] = array[i]; \
+\
+        free(array); \
+        array ## Size /= 2; \
+        array = narr; } \
+
 #endif
